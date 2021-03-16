88faf8ea864e96b0a10bb030a4f99ff26557d668
Alex Hernandez <vlexhndz@gmail.com>
2021-03-16 21:49:57 +0100

    add nginx command

--- api/notifications/admin.py
+++ api/notifications/admin.py
@@ -1,18 +1,18 @@
 from django.contrib import admin
 
-# # Models
-# from api.notifications.models import Notification, NotificationUser
+# Models
+from api.notifications.models import Notification, NotificationUser
 
-# # Register your models here.
-
-
-# @admin.register(Notification)
-# class NotificationAdmin(admin.ModelAdmin):
-#     """Notification model admin."""
-#     pass
+# Register your models here.
 
 
-# @admin.register(NotificationUser)
-# class NotificationUserAdmin(admin.ModelAdmin):
-#     """Notification user model admin."""
-#     pass
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    """Notification model admin."""
+    pass
+
+
+@admin.register(NotificationUser)
+class NotificationUserAdmin(admin.ModelAdmin):
+    """Notification user model admin."""
+    pass
