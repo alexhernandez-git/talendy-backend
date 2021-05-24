3ab1175ca67d5541692da0213d03749d04556a01
Alex Hernandez <vlexhndz@gmail.com>
2021-05-24 13:39:12 +0200

    fewa

--- api/posts/admin.py
+++ api/posts/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Models
-
+from api.posts.models import Post
 
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Connection model admin."""
+
+    list_display = ("id", "title", "user", "status")
