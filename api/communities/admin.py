ccc2c9e458a764bdfacb1939ff6f43d9ce92e511
Alex Hernandez <vlexhndz@gmail.com>
2021-05-29 02:07:17 +0200

    aewffeaw

--- api/communities/admin.py
+++ api/communities/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
 # Models
-
+from api.communities.models import Community
 
 # Register your models here.
+
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    """Community model admin."""
